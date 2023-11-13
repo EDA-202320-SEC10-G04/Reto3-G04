a77fc433ff1d3a722f6b546553d95d224f728531
haespimo <141876495+haespimo@users.noreply.github.com>
2023-11-12 19:44:50 -0500

    avance req6

--- App/model.py
+++ App/model.py
@@ -480,3 +480,15 @@ def getnameTeam(tableList,name):
     for element in lt.iterator(tableList['home_team']):
         if name == element:
             nameTeam.addLast(element)
+
+from math import radians, sin, cos, sqrt, atan2
+
+def getdistance(lon1, lat1, lon2, lat2):
+    R = 6371.0
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = R * c
+    return distance
